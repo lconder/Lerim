9f66198493976e806a17c48b9d6786dcec810c90
lconder <conderodriguez.luis@outlook.com>
2015-08-31 02:34:38 -0500

    Cambios2

--- application/views/Analisis.php
+++ application/views/Analisis.php
@@ -3,13 +3,13 @@
 echo "<h1><strong>Ingrese los valores de los análisis realizados:</strong></h1><br>";
 ?>
 <div class="form-group">
-	<label class="col-md-1 control-label" for="representante">Fecha An&aacute;lisis</label>
+	<label class="col-md-1 control-label" for="fecha_analisis">Fecha An&aacute;lisis</label>
 		<div class="col-sm-3">
-	    	<input id="fecha" name="fecha" type="date" class="form-control"  value=<?php echo  date("Y-m-d")?> max=<?php echo  date("Y-m-d")?> required="">  
+	    	<input id="fecha_analisis" name="fecha_analisis" type="date" class="form-control"  value=<?php echo  date("Y-m-d")?> max=<?php echo  date("Y-m-d")?> required="">  
 	  	</div>
-	  	<label class="col-md-1 control-label" for="representante">Fecha An&aacute;lisis</label>
+	  	<label class="col-md-1 control-label" for="fecha_resultados">Fecha An&aacute;lisis</label>
 		<div class="col-sm-3">
-	    	<input id="fecha" name="fecha" type="date" class="form-control" value=<?php echo  date("Y-m-d")?> max=<?php echo  date("Y-m-d")?> required="">  
+	    	<input id="fecha_resultados" name="fecha_resultados" type="date" class="form-control" value=<?php echo  date("Y-m-d")?> max=<?php echo  date("Y-m-d")?> required="">  
 	  	</div>
 </div>
 <?php
